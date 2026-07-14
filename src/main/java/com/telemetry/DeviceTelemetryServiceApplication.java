15b92ce3a2ecf9335249906106a5e06c77aea41d
burcinozkan <hburcinozkan@gmail.com>
2026-07-14 03:27:59 +0300

    chore: scaffold Spring Boot application

---
+++ src/main/java/com/telemetry/DeviceTelemetryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceTelemetryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceTelemetryServiceApplication.class, args);
+	}
+
+}
