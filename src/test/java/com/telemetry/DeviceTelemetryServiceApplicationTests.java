1f312fefff77bc9a147634b3e6cbffd1b0e4aae5
burcinozkan <hburcinozkan@gmail.com>
2026-07-15 23:59:31 +0300

    fix: use H2 in-memory database for integration tests

--- src/test/java/com/telemetry/DeviceTelemetryServiceApplicationTests.java
+++ src/test/java/com/telemetry/DeviceTelemetryServiceApplicationTests.java
@@ -2,12 +2,18 @@ package com.telemetry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class DeviceTelemetryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
